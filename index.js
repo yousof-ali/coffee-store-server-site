43f17026c7494ef85d5cad64b38714f5a6297d60
yousof-ali <yousofali293@gmail.com>
2024-08-26 17:19:01 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/' , (req,res) => {
+    res.send("coffee store server is running !!!")
+})
+
+
+app.listen(port, () => {
+    console.log(`cofffe server is running on port ${port}`);
+}) 
